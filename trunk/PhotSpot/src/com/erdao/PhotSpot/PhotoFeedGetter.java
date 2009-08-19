38043d3749b6ffc5af0f0e779070efa962803a0e
huan.erdao <huan.erdao@c2c54b48-8119-11de-9ae6-97cb6de55c0b>
2009-08-19 22:12:00 +0000

    for 1.1.3

--- trunk/PhotSpot/src/com/erdao/PhotSpot/PhotoFeedGetter.java
+++ trunk/PhotSpot/src/com/erdao/PhotSpot/PhotoFeedGetter.java
@@ -52,9 +52,9 @@ public class PhotoFeedGetter {
 	private final Context context_;
 	int service_;
 	private ArrayList<PhotoItem> photoItems_ = new ArrayList<PhotoItem>();
-	private String owner_p = "";
-	private double lat_p = 0.0;
-	private double lng_p = 0.0;
+	public final static int CODE_HTTPERROR	= -1;
+	public final static int CODE_NORESULT	= 0;
+	public final static int CODE_OK			= 1;
 
 	/* constructor */
 	public PhotoFeedGetter(int svc, Context context){
@@ -74,7 +74,7 @@ public class PhotoFeedGetter {
 	}
 
 	/* getFeed */
-	public String getFeed(String uri){
+	public Integer getFeed(String uri){
 		final HttpGet get = new HttpGet(uri);
 		HttpEntity entity = null;
 		HttpResponse response;
@@ -85,7 +85,7 @@ public class PhotoFeedGetter {
 			if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
 				entity = response.getEntity();
 				is = entity.getContent();
-				InputStreamReader isr = new InputStreamReader(is);
+				InputStreamReader isr = new InputStreamReader(is,"UTF-8");
 				BufferedReader reader = new BufferedReader(isr,1024);
 				strbuilder = new StringBuilder();
 				String line = null;
@@ -107,102 +107,46 @@ public class PhotoFeedGetter {
 				e.printStackTrace();
 			}
 		}
-		if( strbuilder != null ){
-			String result = strbuilder.toString();
-			JSONObject jsonobj;
-			try {
-				JSONArray array = null;
-				switch( service_ ){
-					case ContentProvider.Panoramio: {
-						jsonobj = new JSONObject(result);
-						array = jsonobj.getJSONArray("photos");
-						break;
-					}
-					case ContentProvider.PicasaWeb: {
-						jsonobj = new JSONObject(result);
-						array = jsonobj.getJSONArray("photos");
-						break;
-					}
-					case ContentProvider.Flickr: {
-						result = result.substring(14, result.length());
-						jsonobj = new JSONObject(result);
-						array = jsonobj.getJSONObject("photos").getJSONArray("photo");
-						break;
-					}
+		if( strbuilder == null )
+			return CODE_HTTPERROR;
+
+		String result = strbuilder.toString();
+		JSONObject jsonobj;
+		try {
+			JSONArray array = null;
+			jsonobj = new JSONObject(result);
+			long feedcount = jsonobj.getLong("count");
+			if(feedcount==0)
+				return CODE_NORESULT;
+			array = jsonobj.getJSONArray("photo");
+			int count = array.length();
+			for (int i = 0; i < count; i++) {
+				if( photoItems_.size() >= 100 )	// max = 100 items
+					break;
+				JSONObject obj = array.getJSONObject(i);
+				long id = 0;
+				String title = null, thumb = null, photoUrl = null, owner= null;
+				double lat = 0.0,lng = 0.0;
+				id = obj.getLong("id");
+				title = obj.getString("title");
+				thumb = obj.getString("thumbUrl");
+				photoUrl = obj.getString("photoUrl");
+				lat = obj.getDouble("lat");
+				lng = obj.getDouble("lng");
+				owner = obj.getString("author");
+				if(title == null || title.length() == 0) {
+					title = context_.getString(R.string.no_title);
 				}
-				int count = array.length();
-				for (int i = 0; i < count; i++) {
-					if( photoItems_.size() >= 100 )	// max = 100 items
-						break;
-					JSONObject obj = array.getJSONObject(i);
-					long id = 0;
-					String title = null, thumb = null, photoUrl = null, owner= null;
-					double lat = 0.0,lng = 0.0;
-					switch( service_ ){
-						case ContentProvider.Panoramio: {
-							id = obj.getLong("photo_id");
-							title = obj.getString("photo_title");
-							thumb = obj.getString("photo_file_url");
-							photoUrl = obj.getString("photo_url");
-							lat = obj.getDouble("latitude");
-							lng = obj.getDouble("longitude");
-							owner = obj.getString("owner_name");
-							break;
-						}
-						case ContentProvider.PicasaWeb: {
-							id = obj.getLong("id");
-							title = obj.getString("title");
-							JSONObject mediaobj = obj.getJSONObject("media");
-							JSONObject linkobj = obj.getJSONObject("links");
-							thumb = mediaobj.getJSONArray("thumbnails").getString(2);
-							photoUrl = linkobj.getString("alternate");
-							String latlng = obj.getJSONObject("geo").getString("point");
-							String token[] = latlng.split(" ");
-							lat = Double.valueOf(token[0]);
-							lng = Double.valueOf(token[1]);
-							owner = obj.getString("author");
-							break;
-						}
-						case ContentProvider.Flickr: {
-							String server = obj.getString("server");
-							String secret = obj.getString("secret");
-							id = obj.getLong("id");
-							title = obj.getString("title");
-							thumb = "http://static.flickr.com/"+server+"/"+id+"_"+secret+"_m.jpg";
-							photoUrl = "http://www.flickr.com/photos/"+obj.getString("owner")+"/"+id;
-							lat = obj.getDouble("latitude");
-							lng = obj.getDouble("longitude");
-							owner = obj.getString("owner");	// todo:get real user name..
-							break;
-						}
-					}
-					if(title == null || title.length() == 0) {
-						title = context_.getString(R.string.no_title);
-					}
-					/* apply some filter to eliminate content with same owner with exact same location
-					 * wish to compare timestamp also, but flickr cannot retrieve timestamp in same feed,
-					 * so the time comparison is not yet implemented.
-					 * todo: range check of location, timestamp comparison
-					 */
-					if(owner.contentEquals(owner_p)){
-						if((lat<(lat_p-0.001))&&(lat>(lat_p+0.001))||(lng<(lng_p-0.001))&&(lng>(lng_p+0.001))){
-							continue;
-						}
-					}
-					PhotoItem item =
-						new PhotoItem(id,thumb,(int)(lat*1E6),(int)(lng*1E6),title,photoUrl,owner);
-					photoItems_.add(item);
-					owner_p = owner;
-					lat_p = lat;
-					lng_p = lng;
-				}
-				return result; 
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				PhotoItem item =
+					new PhotoItem(id,thumb,(int)(lat*1E6),(int)(lng*1E6),title,photoUrl,owner);
+				photoItems_.add(item);
 			}
+			return CODE_OK; 
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		return null;
+		return CODE_NORESULT;
 	}
 	
 	/* getPhotoItemList */
