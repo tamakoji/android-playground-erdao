d5d6ec31a4089e3c2468a40d55a49513b766d3d8
huan.erdao <huan.erdao@c2c54b48-8119-11de-9ae6-97cb6de55c0b>
2009-10-06 23:17:48 +0000

    ver 1.5.1

---
+++ 1.5.1/src/com/erdao/PhotSpot/FavoritesGalleryActivity.java
@@ -0,0 +1,222 @@
+/*
+ * Copyright (C) 2009 Huan Erdao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.erdao.PhotSpot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.AdapterView;
+import android.widget.Gallery;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+import com.erdao.utils.LazyLoadBitmap;
+import com.erdao.utils.LazyLoadBitmap.LoadState;
+import com.google.android.maps.GeoPoint;
+
+/**
+ * Activity Class for Displaying Favorites in Gallery View.
+ * @author Huan Erdao
+ */
+public class FavoritesGalleryActivity extends Activity {
+	
+	/** PhotSpotDBHelper object */
+	private PhotSpotDBHelper dbHelper_ = null;
+	/** PhotoItem list */
+    private List<PhotoItem> photoItems_ = new ArrayList<PhotoItem>();
+	/** Bitmap list */
+    private List<LazyLoadBitmap> bitmaps_ = new ArrayList<LazyLoadBitmap>();
+	/** Context object */
+	private Context context_;
+	/** ImageListAdapter for gallery */
+	private ImageListAdapter imageAdapter_ = null;
+	/** current selected item */
+	private int selItem_;
+	
+	/** Extra Action - Show on Map */
+	private static final int EXT_ACTION_SHOWONMAP			= 0;
+	/** Extra Action - Navigate to place */
+	private static final int EXT_ACTION_NAVTOPLACE			= 1;
+	/** Extra Action - Open with Browser */
+	private static final int EXT_ACTION_OPENBROWSER			= 2;
+
+
+	/**
+	 * onCreate handler.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		context_ = this;
+		selItem_ = 0;
+		dbHelper_ = new PhotSpotDBHelper(this);
+		final SQLiteDatabase db = dbHelper_.getReadableDatabase();
+		Cursor c = dbHelper_.queryAllItems(db);
+		c.moveToFirst();
+		for (int i = 0; i < c.getCount(); i++) {
+            long id = c.getLong(PhotSpotDBHelper.Spots.IDX_ID);
+			String title = c.getString(PhotSpotDBHelper.Spots.IDX_TITLE);
+			String author = c.getString(PhotSpotDBHelper.Spots.IDX_AUTHOR);
+			String thumbUrl = c.getString(PhotSpotDBHelper.Spots.IDX_THUMB_URL);
+			String photoUrl = c.getString(PhotSpotDBHelper.Spots.IDX_PHOTO_URL);
+			double lat = c.getDouble(PhotSpotDBHelper.Spots.IDX_LATITUDE);
+			double lng = c.getDouble(PhotSpotDBHelper.Spots.IDX_LONGITUDE);
+			long labelId = c.getLong(PhotSpotDBHelper.Spots.IDX_LABEL);
+			PhotoItem item =
+				new PhotoItem(id,(int)(lat*1E6),(int)(lng*1E6),title,author,thumbUrl,photoUrl);
+			item.setLabelId(labelId);
+			photoItems_.add(item);
+ 			Bitmap bitmap = null;
+			byte[] bmpStream = c.getBlob(PhotSpotDBHelper.Spots.IDX_THUMBDATA);
+			if(bmpStream!=null){
+				bitmap = BitmapFactory.decodeByteArray(bmpStream, 0, bmpStream.length);
+				bitmaps_.add(new LazyLoadBitmap(bitmap,LoadState.BITMAP_STATE_FULL_LOADED));
+			}
+			else{
+				bitmaps_.add(new LazyLoadBitmap());
+			}
+			c.moveToNext();
+		}
+		c.close();
+		db.close();
+		if(photoItems_.isEmpty())
+			setContentView(R.layout.fav_noitem);
+		else{
+			setContentView(R.layout.fav_gallery);
+			imageAdapter_ = new ImageListAdapter(context_,photoItems_,bitmaps_);
+			imageAdapter_.setSize(LayoutParams.FILL_PARENT,LayoutParams.FILL_PARENT);
+			Gallery gallery = (Gallery)findViewById(R.id.fav_gallery);
+			gallery.setAdapter(imageAdapter_);
+			gallery.setCallbackDuringFling(true);
+			TextView txtView = (TextView)findViewById(R.id.fav_imgdesc);
+			txtView.setText(imageAdapter_.getDescription(selItem_));
+			photoItems_.get(selItem_).setSelect(true);
+			gallery.setSelection(selItem_);
+			gallery.setOnItemSelectedListener(new OnItemSelectedListener() {
+				public void onItemSelected (AdapterView<?> parent, View view, int position, long id){
+					photoItems_.get(selItem_).setSelect(false);
+					photoItems_.get(position).setSelect(true);
+					selItem_ = position;
+					TextView txtView = (TextView)findViewById(R.id.fav_imgdesc);
+					txtView.setText(imageAdapter_.getDescription(position));
+				}
+				public void onNothingSelected (AdapterView<?> parent){
+				}
+			});
+			gallery.setOnItemClickListener(new OnItemClickListener() {
+				public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+					new AlertDialog.Builder(context_)
+					.setTitle(R.string.ExtActionDlgTitle)
+					.setPositiveButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int whichButton) {
+							dialog.dismiss();
+						}
+					})
+					.setItems(R.array.favorite_gallery_action, new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {
+							onItemAction(which);
+							dialog.dismiss();
+						}
+					})
+				   .create()
+				   .show();
+				}
+			});
+		}
+	}
+
+	
+	/**
+	 * option tasks for long pressing item
+	 * @param cmd action command
+	 */
+	public void onItemAction(int cmd){
+		switch(cmd){
+			case EXT_ACTION_SHOWONMAP:{
+				PhotoItem item = photoItems_.get(selItem_);
+	            Intent intent = new Intent(this, PhotSpotActivity.class);
+	            intent.setAction(Intent.ACTION_VIEW);
+	            item.setBitmap(bitmaps_.get(selItem_).getBitmap());
+	            intent.putExtra(PhotoItem.EXT_PHOTOITEM, item);
+	            startActivity(intent);
+//	            finish();
+	            break;
+			}
+			case EXT_ACTION_NAVTOPLACE:{
+				PhotoItem item = photoItems_.get(selItem_);
+				GeoPoint location = item.getLocation();
+				double lat = location.getLatitudeE6()/1E6;
+				double lng = location.getLongitudeE6()/1E6;
+				String url = "http://maps.google.com/maps?daddr="+lat+","+lng;
+				Intent i = new Intent(Intent.ACTION_VIEW,Uri.parse(url));
+				i.addCategory(Intent.CATEGORY_BROWSABLE);
+				i.setFlags(0x2800000);
+				context_.startActivity(i);
+				break;
+			}
+			case EXT_ACTION_OPENBROWSER:{
+				PhotoItem item = photoItems_.get(selItem_);
+				String url = item.getPhotoUrl();
+				Intent i = new Intent(Intent.ACTION_VIEW,Uri.parse(url));
+				context_.startActivity(i);
+				break;
+			}
+		}
+	}
+
+	/**
+	 * onCreateOptionsMenu handler
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuItem menu_Revert = menu.add(0,R.id.menu_Revert,0,R.string.menu_Revert);
+		menu_Revert.setIcon(android.R.drawable.ic_menu_revert);
+		return true;
+	}
+
+	/**
+	 * onOptionsItemSelected handler
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.menu_Revert: {
+				finish();
+				break;
+			}
+		}
+		return true;
+	}
+
+}
---
+++ 1.5.1/src/com/erdao/PhotSpot/JsonFeedGetter.java
@@ -0,0 +1,214 @@
+/*
+ * Copyright (C) 2009 Huan Erdao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.erdao.PhotSpot;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+
+/**
+ * JSON Feed get and parse utility
+ * @author Huan Erdao
+ */
+public class JsonFeedGetter {
+	/* constant */
+	public static final int MODE_SPOTSEARCH		= 0;
+	public static final int MODE_LOCALSEARCH	= 1;
+	public static final int MODE_FLICKR_ID		= 2;
+	public static final int MODE_PICASA_ID		= 3;
+
+	public final static int CODE_HTTPERROR		= -2;
+	public final static int CODE_JSONERROR		= -1;
+	public final static int CODE_NORESULT		= 0;
+	public final static int CODE_OK				= 1;
+
+	/** IO BUFFER SIZE = 2M */
+	private final static int IO_BUFFER_SIZE = 2 * 1024;
+
+	/* variables */
+	private HttpClient httpClient_;
+	private final int connection_Timeout = 10000;
+	private final Context context_;
+	int mode_;
+	private List<PhotoItem> photoItems_ = new ArrayList<PhotoItem>();
+	private List<CharSequence> localSpots_ = new ArrayList<CharSequence>();
+	private String userid_ = "";
+
+	/* constructor */
+	public JsonFeedGetter(Context c, int mode){
+		mode_ = mode;
+		context_ = c;
+		final HttpParams httpParams = new BasicHttpParams();
+		HttpProtocolParams.setVersion(httpParams, HttpVersion.HTTP_1_1);
+		HttpProtocolParams.setContentCharset(httpParams, "UTF-8");
+		HttpConnectionParams.setConnectionTimeout(httpParams,connection_Timeout); 
+		HttpConnectionParams.setSoTimeout(httpParams,connection_Timeout); 
+		final SchemeRegistry schemeRegistry = new SchemeRegistry();
+		schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+		httpClient_ = new DefaultHttpClient(
+				new ThreadSafeClientConnManager(httpParams, schemeRegistry),
+				httpParams);
+		
+	}
+
+	/* getFeed */
+	public Integer getFeed(String uri){
+		final HttpGet get = new HttpGet(uri);
+		HttpEntity entity = null;
+		HttpResponse response;
+		StringBuilder strbuilder = null;
+		InputStream is = null;
+		boolean isSuccess = false;
+		try {
+			response = httpClient_.execute(get);
+			if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+				entity = response.getEntity();
+				is = entity.getContent();
+				InputStreamReader isr = new InputStreamReader(is,"UTF-8");
+				BufferedReader reader = new BufferedReader(isr,IO_BUFFER_SIZE);
+				strbuilder = new StringBuilder();
+				String line = null;
+				while ((line = reader.readLine()) != null) {
+					strbuilder.append(line + "\n");
+				}
+				isSuccess = true;
+			}
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				if(is!=null)
+					is.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		if( strbuilder == null ){
+			if(mode_==MODE_PICASA_ID&&!isSuccess)
+				return CODE_JSONERROR;
+			return CODE_HTTPERROR;
+		}
+
+		String result = strbuilder.toString();
+		JSONObject jsonobj = null;
+		try {
+			JSONArray array = null;
+			switch(mode_){
+				default:
+				case MODE_SPOTSEARCH:{
+					jsonobj = new JSONObject(result);
+					array = jsonobj.getJSONArray("photo");
+					break;
+				}
+				case MODE_LOCALSEARCH:{
+					jsonobj = new JSONObject(result);
+					array = jsonobj.getJSONArray("result");
+					break;
+				}
+				case MODE_FLICKR_ID:{
+					result = result.substring(14, result.length());
+					jsonobj = new JSONObject(result);
+					jsonobj = jsonobj.getJSONObject("user");
+					userid_ = jsonobj.getString("nsid");
+					return CODE_OK; 
+				}
+				case MODE_PICASA_ID:{
+					return CODE_OK; 
+				}
+			}
+			long feedcount = jsonobj.getLong("count");
+			if(feedcount==0)
+				return CODE_NORESULT;
+			int count = array.length();
+			for (int i = 0; i < count; i++) {
+				JSONObject obj = array.getJSONObject(i);
+				long id = 0;
+				String title = null, thumbUrl = null, photoUrl = null, author= null;
+				double lat = 0.0,lng = 0.0;
+				if(mode_==MODE_SPOTSEARCH){
+					id = obj.getLong("id");
+					title = obj.getString("title");
+					thumbUrl = obj.getString("thumbUrl");
+					photoUrl = obj.getString("photoUrl");
+					lat = obj.getDouble("lat");
+					lng = obj.getDouble("lng");
+					author = obj.getString("author");
+					if(title == null || title.length() == 0) {
+						title = context_.getString(R.string.no_title);
+					}
+					PhotoItem item =
+						new PhotoItem(id,(int)(lat*1E6),(int)(lng*1E6),title,author,thumbUrl,photoUrl);
+					photoItems_.add(item);
+				}
+				else{
+					title = obj.getString("title");
+					localSpots_.add(title);
+				}
+			}
+			return CODE_OK; 
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return CODE_JSONERROR;
+	}
+	
+	/* getPhotoItemList */
+	public List<PhotoItem> getPhotoItemList(){
+		return photoItems_;
+	}
+
+	/* getLocalSpotsList */
+	public List<CharSequence> getLocalSpotsList(){
+		return localSpots_;
+	}
+
+	/* getUserId */
+	public final String getUserId(){
+		return userid_;
+	}
+
+}
