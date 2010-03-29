84763204fb6fafe239ba3c01b4b6a68086eb1af4
huan.erdao@gmail.com <huan.erdao@gmail.com@c2c54b48-8119-11de-9ae6-97cb6de55c0b>
2010-03-29 04:41:53 +0000

    modified comments and copyrights

--- trunk/LaughingMan/src/com/erdao/android/LaughingMan/LaughingManActivity.java
+++ trunk/LaughingMan/src/com/erdao/android/LaughingMan/LaughingManActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009 Huan Erdao
+ * Copyright (C) 2010 Huan Erdao
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
